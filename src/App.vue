48b8fc576f8ab8af892fdc1cbfb052880da98552
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-14 22:18:18 +0800

    Restart with a new vite vue-ts project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
-<script>
-import { defineComponent } from "vue";
-import { NMessageProvider } from "naive-ui";
-import Header from "./components/Header.vue";
-
-export default defineComponent({
-  components: {
-    Header,
-    NMessageProvider,
-  },
-});
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <n-message-provider>
-    <Header></Header>
-    <router-view></router-view>
-  </n-message-provider>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
