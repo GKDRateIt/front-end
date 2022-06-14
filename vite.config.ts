507a6f82bf49944481f853524e0511cbadf3622e
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-14 23:59:00 +0800

    Add eslint support; Fix auto reload on wsl2; Create page header;

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import liveReload from "vite-plugin-live-reload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  server: {
+    watch: {
+      // Hack for WSL2
+      usePolling: true,
+    },
+  },
+  plugins: [vue()],
+});
