302e528d2799605ae05793707ea32a1547e55c7d
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-15 00:11:24 +0800

    Basic page structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import PageHeader from "./components/PageHeader.vue";
+import MainBody from "./components/MainBody.vue";
+import PageFooter from "./components/PageFooter.vue";
 </script>
 
 <template>
   <page-header />
+  <main-body />
+  <page-footer />
 </template>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="footer" class="bg-neutral-200 w-full h-14 bottom-0 absolute">
+    <div class="ml-5">
+      <div class="flex space-x-5">
+        <div>关于我们</div>
+        <div>意见反馈</div>
+      </div>
+      <div>copyright</div>
+    </div>
+  </div>
+</template>
