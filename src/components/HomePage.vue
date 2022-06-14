d017730479a082384557352436144a4371c1e9f8
LinHeLurking <LinHe.Lurking@gmail.com>
2022-06-15 01:40:58 +0800

    VS Code settings; Homepage structure;

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import SearchBox from "./SearchBox.vue";
+import { NButton } from "naive-ui";
+</script>
+
 <template>
-  <div class="h-auto">
-    <div class="m-auto inset-1/2 text-center">PLACEHOLDER</div>
+  <div class="flex-col m-auto inset-1/2 mt-[23vh] space-y-6">
+    <div class="flex mx-auto w-fit space-x-5">
+      <div class="text-7xl h-fit mt-auto mb-0">RateIt</div>
+      <div class="text-6xl h-fit mt-auto mb-0">@UCAS</div>
+    </div>
+    <div class="w-fit m-auto">
+      <search-box />
+    </div>
+    <div class="flex m-auto w-fit space-x-5">
+      <n-button> 开始检索 </n-button>
+      <n-button> 试试手气 </n-button>
+    </div>
   </div>
 </template>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, Ref, computed } from "vue";
+import { NAutoComplete } from "naive-ui";
+
+const inputData: Ref<string> = ref("");
+
+const submitSearch = computed(() => {
+  console.log("Searching " + inputData.value);
+  return [
+    {
+      type: "group",
+      label: "教师",
+      key: 1,
+      children: ["123", "456"],
+    },
+    {
+      type: "group",
+      label: "课程",
+      key: 2,
+      children: ["abc", "def"],
+    },
+  ];
+});
+</script>
+
+<template>
+  <div class="w-[45vw]">
+    <n-auto-complete
+      v-model:value="inputData"
+      size="large"
+      placeholder="输入你想查询的课程名称或主讲教师"
+      :options="submitSearch"
+    />
+  </div>
+</template>
