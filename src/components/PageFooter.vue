74f661dda9eb73fc671541ebaacf0fa475710eac
LinHeLurking <LinHe.Lurking@gmail.com>
2022-08-26 23:27:42 +0800

    Feedback & Contact Us;

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,10 +3,7 @@
     <div class="ml-5">
       <div class="flex space-x-5">
         <div>
-          <router-link to="/about"> 关于我们 </router-link>
-        </div>
-        <div>
-          <router-link to="/feedback">意见反馈</router-link>
+          <router-link to="/about"> 关于我们/意见反馈 </router-link>
         </div>
       </div>
       <div>copyright</div>
