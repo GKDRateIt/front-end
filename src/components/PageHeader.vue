932d4027c1721a5e019e896e1ab8929c03ce84f0
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-22 22:30:21 +0800

    Search bar in header;

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, computed, ref, Ref } from "vue";
 import { Icon } from "@iconify/vue";
 import uiUserProfile from "@iconify-icons/healthicons/ui-user-profile-negative";
 import stackIcon from "@iconify-icons/charm/stack";
 import { useSideBar } from "./side_bar/sideBarApi";
-import { useWindowInfo } from "../util";
+import { useWindowInfo, wrapStyleClass } from "../util";
 import UserProfile from "./user/UserProfile.vue";
-import { computed } from "vue";
+import { NAutoComplete } from "naive-ui";
 
 const { isLoggedIn } = inject("isLoggedIn") as { isLoggedIn: any };
 
@@ -27,27 +27,58 @@ const headerBorderStyle = computed(() => {
   }
   return style;
 });
+
+const searchBarStatus: Ref<"focus" | "blur"> = ref("blur");
+
+const searchBarStyleClass = computed(() => {
+  if (searchBarStatus.value == "focus") {
+    return "w-[100%]";
+  } else if (searchBarStatus.value == "blur") {
+    if (windowInfo.value.isNarrow) {
+      return "w-[100%]";
+    } else {
+      return "w-[50%]";
+    }
+  }
+  return "";
+});
 </script>
 
 <template>
   <div id="header" class="fixed top-0 w-full" :style="headerBorderStyle">
     <div class="bg-[#2755a5] w-full h-14 flex justify-between">
       <!-- Left part -->
-      <div class="w-fit flex justify-start space-x-6 ml-5">
+      <div class="min-w-[350px] w-1/2 flex justify-start space-x-6 pl-5">
         <div
-          class="flex h-full items-center cursor-pointer"
+          class="flex w-fit h-full items-center cursor-pointer"
           @click="mainIconClick"
         >
           <Icon :icon="stackIcon" width="30" style="color: white" />
         </div>
         <router-link to="/">
-          <div class="flex h-full items-center cursor-pointer">
+          <div class="flex w-fit h-full items-center cursor-pointer">
             <div class="text-4xl text-white">RateIt</div>
           </div>
         </router-link>
+        <div class="flex h-full items-center w-2/3">
+          <n-auto-complete
+            class="transition-all ease-in-out duration-200"
+            :class="searchBarStyleClass"
+            @focus="
+              () => {
+                searchBarStatus = 'focus';
+              }
+            "
+            @blur="
+              () => {
+                searchBarStatus = 'blur';
+              }
+            "
+          />
+        </div>
       </div>
       <!-- Right part -->
-      <div class="w-fit flex justify-end space-x-6 mr-6 my-3">
+      <div class="w-fit flex justify-end space-x-6 pr-6 my-3">
         <div v-if="isLoggedIn">
           <user-profile />
         </div>
