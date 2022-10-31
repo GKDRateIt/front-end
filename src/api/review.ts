288c855d61db2f66d9fed56a07f46d9975019c80
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-31 17:14:36 +0800

    My review page;

--- src/api/review.ts
+++ src/api/review.ts
@@ -27,17 +27,30 @@ export interface ReviewCreateQuery {
   commentText: string;
 }
 
+export interface ReviewReadQuery {
+  courseId?: string;
+  email?: string;
+  reviewId?: string;
+  userId?: string;
+}
+
 export class ReviewApi {
   // post to `${apiPrefix}/api/review`.
-  public static async getReviewsByCourseId(
-    courseId: number
+  public static async getReviews(
+    readQuery: ReviewReadQuery
   ): Promise<ReviewModel[]> {
     const responseBody = await fetch(`${apiPrefix}/api/review`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ _action: "read", courseId: courseId.toString() }),
+      body: JSON.stringify({
+        _action: "read",
+        courseId: readQuery.courseId,
+        email: readQuery.email,
+        reviewId: readQuery.reviewId,
+        userId: readQuery.userId,
+      }),
     });
     const response = (await responseBody.json()) as ApiResponse<ReviewModel[]>;
     if (response.data) {
@@ -45,6 +58,25 @@ export class ReviewApi {
     } else {
       return [];
     }
+  }
+  // post to `${apiPrefix}/api/review`.
+  public static async getReviewsByCourseId(
+    courseId: number
+  ): Promise<ReviewModel[]> {
+    return this.getReviews({ courseId: courseId.toString() });
+  }
+
+  // post to `${apiPrefix}/api/review`
+  public static async getReviewsByUserId(
+    userId: number
+  ): Promise<ReviewModel[]> {
+    return this.getReviews({ userId: userId.toString() });
+  }
+
+  public static async getReviewsByUserEmail(
+    email: string
+  ): Promise<ReviewModel[]> {
+    return this.getReviews({ email: email });
   }
 
   // post to `${apiPrefix}/api/review` to create new reivew.
--- src/components/user/MyReviews.vue
+++ src/components/user/MyReviews.vue
@@ -1,1 +1,27 @@
-<template>(WIP) 我的评价</template>
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+import { ReviewModel, ReviewApi } from "../../api/review";
+import { UserApi } from "../../api/user";
+
+const email = UserApi.getLoggedInUserEmail();
+
+const reviews: Ref<ReviewModel[]> = ref([]);
+
+if (email) {
+  ReviewApi.getReviewsByUserEmail(email).then((res: ReviewModel[]) => {
+    for (let review of res) {
+      reviews.value.push(review);
+    }
+  });
+}
+</script>
+
+<template>
+  <div>
+    <div>(WIP) 我的评价</div>
+    <div v-for="(review, idx) in reviews" :key="idx" class="mt-5">
+      <div>用户 ID：{{ review.userId }}</div>
+      <div>评价：{{ review.commentText }}</div>
+    </div>
+  </div>
+</template>
