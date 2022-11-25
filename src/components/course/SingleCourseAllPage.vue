47c6d8da4259d98a1cbf7091d35a0e35cc73094b
LinHeLurking <LinHe.Lurking@gmail.com>
2022-11-25 20:48:01 +0800

    fix: course api type issue;

--- src/components/course/SingleCourseAllPage.vue
+++ src/components/course/SingleCourseAllPage.vue
@@ -10,9 +10,7 @@ const router = useRouter();
 const route = useRoute();
 
 const courseCode = String(route.query.code);
-const courseSeq: string | null = route.query.seq
-  ? route.query.seq.toString()
-  : null;
+const courseSeq = route.query.seq ? route.query.seq.toString() : undefined;
 
 const teacher: Ref<TeacherModel | null> = ref(null);
 const course: Ref<CourseModel | null> = ref(null);
